2376e49ef9579a68ed90791680a7f65020969b29
Valeriia Lysenko <valeriia.lysenko@globallogic.com>
2024-11-26 13:41:19 +0200

    Refactor existing confirmation dialog without Angular Material dialog and dialog.service

--- src/app/components/confirm/confirm.component.ts
+++ src/app/components/confirm/confirm.component.ts
@@ -1,31 +1,39 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  MAT_DIALOG_DATA,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
-} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-confirm',
   standalone: true,
-  imports: [MatButtonModule, MatDialogActions,
-    MatDialogClose, MatDialogTitle, MatDialogContent],
+  imports: [MatButtonModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './confirm.component.html',
   styleUrl: './confirm.component.scss'
 })
 export class ConfirmComponent {
-  readonly data = inject(MAT_DIALOG_DATA);
-  readonly dialogRef = inject(MatDialogRef<ConfirmComponent>);
+  @Input() title!: string;
+  @Input() message!: string;
+  @Input() confirmButtonLabel = 'YES';
+  @Input() cancelButtonLabel = 'NO';
 
-  onCancel(): void {
-    this.dialogRef.close(false);
+  @Input() entityNameToConfirm?: string = '';
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  entityName: string = '';
+
+  get isEntityNameValid(): boolean {
+    return this.entityName === this.entityNameToConfirm;
   }
 
-  onConfirm(): void {
-    this.dialogRef.close(true);
+  onConfirm() {
+    if (this.isEntityNameValid) {
+      this.confirm.emit();
+    }
+  }
+
+  onCancel() {
+    this.cancel.emit();
   }
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -6,25 +6,26 @@ import { AppState } from '../../store';
 import { deleteUser, loadUsers } from '../../store/actions/user.actions';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from '../../components/list/list.component';
-import { DialogService } from '../../services/dialog.service';
-import { Subject, filter, takeUntil } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ConfirmComponent } from "../../components/confirm/confirm.component";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, ListComponent,  MatIconModule, MatButtonModule],
+  imports: [CommonModule, ListComponent, MatIconModule, MatButtonModule, ConfirmComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 
 export class UsersComponent implements OnInit, OnDestroy {
   private readonly store: Store<AppState> = inject(Store);
-  private readonly dialogService = inject(DialogService);
   private destroy$ = new Subject<void>();
 
   users: User[] = [];
+  currentUser: User | null = null;
+  showConfirmDialog = false;
 
   ngOnInit(): void {
     this.store.dispatch(loadUsers());
@@ -33,13 +34,17 @@ export class UsersComponent implements OnInit, OnDestroy {
     .subscribe(res => this.users = res);
   }
 
-  deleteUser(userId: number): void {
-    this.dialogService.openDialog('Delete User',
-    'Would you like to delete user?')
-    .pipe(
-      takeUntil(this.destroy$),
-      filter(res => res))
-    .subscribe(() => this.store.dispatch(deleteUser({ userId })))
+  openConfirmDialog(user: User): void {
+    this.currentUser = user;
+    this.showConfirmDialog = true;
+  }
+
+  deleteUser(isConfirmed: boolean): void {
+    if(isConfirmed) {
+      this.store.dispatch(deleteUser({ userId: this.currentUser!.id }));
+    }
+    this.showConfirmDialog = false;
+    this.currentUser = null;
   }
 
   ngOnDestroy(): void {
